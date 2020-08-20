d65f685c1e35a546528f537e0da41add08ef57c6
jonathancsr <jonathan.candido@natahouse.com>
2020-08-20 19:59:20 -0300

    add: find class

---
+++ Astar/src/Find.ts
@@ -0,0 +1,10 @@
+import No from "./class/No";
+
+export default class Find {
+  private citys: No[];
+  constructor(citys: No[]) {
+    this.citys = citys;
+  }
+
+  private findMinPath(from: No, to: No) {}
+}
